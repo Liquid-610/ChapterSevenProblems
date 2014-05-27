96ed522fabf6c36bd4201195a1dcc7a328a1f86e
Liquid-610 <jamesbarnes71@gmail.com>
2014-05-27 10:00:45 -0400

    started

---
+++ TestPizzas/TestPizzas/Pie.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestPizzas
+{
+    class Pie
+    {
+        private double Diameter;
+        
+        private string Name;
+        private double Price;
+
+        public double diameter
+        {
+            get
+            {
+                return diameter;
+            }
+
+            set
+            {
+                diameter = value;
+            }
+        }
+
+              public double price
+              {
+            get
+            {
+                return price;
+            }
+
+            set 
+            {
+                price = value;
+            }
+              }
+
+              public string name
+              {
+                  get
+                  {
+                      return name;
+                  }
+
+                  set
+                  {
+                      name = value;
+                  }
+              }
+    
+
+        }
+
+    }
+
---
+++ TestPizzas/TestPizzas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestPizzas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pie pizza = new Pie();
+
+            double d = pizza.diameter;
+            double p = pizza.price;
+
+            pizza.price = 13.99;
+            pizza.diameter = 12;
+            pizza.name = "Pepperoni";
+
+            Console.WriteLine("{0}in",pizza.diameter);
+            Console.WriteLine(pizza.name);
+            Console.WriteLine(pizza.price);
+
+        }
+    }
+}
